f8f2601c3ce8a454c353a2293c3bc2d5b9c6752f
gcmsw <grace.chiang@sjsu.edu>
2025-06-21 11:19:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from commands import ReviewButtons
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
